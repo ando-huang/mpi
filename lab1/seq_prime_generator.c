93b5d3d5cd4c68e7729c48f981d22ce31cb3509a
ando-huang <ahuang09182000@gmail.com>
2020-10-28 22:42:32 -0400

    needs argument fix

--- lab1/seq_prime_generator.c
+++ lab1/seq_prime_generator.c
@@ -12,19 +12,55 @@
 
 const static int MAX = 10000000;
 
-int main(int argc, char* argv[]){
-    int n = (int)argv[1];
+/**
+ * approx size returns a guideline int for the number of primes 
+ * param n must be <= 10000000 and greater than 2
+ */ 
+int approx_size(int n){
+    int size;
+    if(n <= MAX)    { size = 664579;}
+    if(n <= MAX/10) { size = 78498; }
+    if(n <= MAX/100){ size = 9592;  }
+    if(n <= MAX/1000){size = 1229;  }
+    if(n <= 1000)   { size = 168;   }
+    if(n <= 100)    { size = 25;    }
+    if(n <= 10)     { size = 4;     }
+    return size;
+}
+
+int* gen_prime_list(int n){
+    int size = approx_size(n);
+    int primes[size]; //maybe change the initial size of the array to match whatever the size is expected to be
+    primes[0] = 2;
+    int count = 1;
+    for(int i = 2; i < n; i++){
+        int isPrime = 0; //is prime
+        for(int j = 0; j < count; j++){
+            if(i % primes[j] == 0){ isPrime = 1; }
+        }
+        if (isPrime == 0){
+            primes[count] = i;
+            count += 1;
+        }
+    }
+    int* items = {&primes, &count};
+    return &items;
+}
+
+int main(int argc, char** argv[]){
+    int n = *(int*)argv[1]; //input n
 
     if(n > MAX || n < 2){
-        printf("invalid input.");
+        printf("invalid input.\n");
         return -1;
     }//input handling complete
     
     //generate all the primes to a list
-    int* primes = malloc(sizeof(int) * n);
-
-
-
-
+    int* list = gen_prime_list(n);
+    int* primes = list[0];
+    int size = list[1];
+    for(int i = 0; i < size; i++){
+        printf("%d ", primes[i]);
+    }
     return 0;
 }
