53f1a42db87926ce59f05a74cb9dfae9fb58995a
ando-huang <ahuang09182000@gmail.com>
2020-11-05 10:44:14 -0500

    fixed verification error

--- lab1/tests/prime_gen.c
+++ lab1/tests/prime_gen.c
@@ -5,9 +5,11 @@ int* prime_list(int n);
 
 int main(int argc, char* argv[]){
     int n = atoi(argv[1]); //cli arg turned to integer
+    
     int* primes = prime_list(n);
     int count = primes[0];
-    for(int i = 1; i<count; i++){
+
+    for(int i = 1; i<=count; i++){ //this part will be a fprintf in the mpi
         printf("%d\n", primes[i]);
     }
 
@@ -16,21 +18,21 @@ int main(int argc, char* argv[]){
 
 //This function generates the primes in the list
 int* prime_list(int n){
-    int* primes = (int*)(malloc(sizeof(int)*n/2));
-    primes[0] = 2;
-    int count = 0;
+    int* primes = (int*)(malloc(sizeof(int)*(n))); //creates a container of size n/2
+    primes[1] = 2;
+    int count = 1;//current size of the list
     int isPrime;
     for(int i = 2; i <= n; i++){
         isPrime = 0;
-        for(int j = 0; j < count; j++){
-            if(i % primes[j] == 0){
-                isPrime = 1;
+        for(int j = 1; j <= count; j++){
+            if( i % primes[j] == 0){ //if composite
+                isPrime = 1; //indicate not prime
                 break;
             }
         }
-        if(isPrime == 0){
-            primes[count+1] = i;
-            count+=1;
+        if (isPrime == 0){
+            count++;
+            primes[count] = i;
         }
     }
     primes[0] = count;
