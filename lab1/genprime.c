42a0afe911d3218e79d9ca78163d566fdde57097
ando-huang <ahuang09182000@gmail.com>
2020-11-06 01:42:47 -0500

    needs to be updated to match given algo

--- lab1/genprime.c
+++ lab1/genprime.c
@@ -1,14 +1,44 @@
 /**
- *  **** Rename this file to genprime.c ****
- * 
- * 
+ *  @ando-huang
  *  this is the parallel implementation of seq_prime_generator.c
- * 
 **/
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(char* argv){
-    //parse argv for ints
+void write_to_file(int* primes, char* n); //writes the primes into a file named "{n}.txt"
+
+int main(int argc, char* argv[]){
+    int my_rank, comm_sz;  //vars used to organize threads
+    int n = atoi(argv[1]); //now we have N, upper limit of primes to find
     
+    MPI_Init(NULL, NULL); //everything from now on is MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    int start = my_rank * n//comm_sz;
+    int end = start + n//comm_sz;
+
+    for(int i = start; i < end; i++){
+        
+    
+    }
+
+
+    MPI_Finalize();
+
+    //write the array to a file
+    write_to_file(primes, count, argv[1])
+
+    return 0;
+}
+
+//for later use in sequential write to the folder
+void write_to_file(int* primes, int count, char* n){
+    FILE* f = fopen("", "w"); //fix this so that it appends char* n 
+    for(int i = 0; i< count; i++){
+        fprintf(f, "%d ", primes[i]);
+    }
+    f.close(); //close the file, we are done with it
+    return;
 }
---
+++ lab1/tests/prime_gen.py
@@ -0,0 +1,17 @@
+#This file reads in N and generates all primes up to and including N
+n = int(input())
+# this program is O(n^2), not very good but it works
+def prime_gen(n):
+    primes = []
+    for i in range(2, n+1):
+        isPrime = True
+        for j in range(len(primes)):
+            if i%primes[j] == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(i)
+    return primes
+
+primes = prime_gen(n)
+print(primes)
